ab7ff32ce65a294c42b51e4d6c129e613f859b16
guidoch <guido.maresca@iconsolutions.com>
2019-07-19 16:23:22 +0100

    lol

--- src/main/java/com/paltaie/romannumerals/RomanNumeralServiceImpl.java
+++ src/main/java/com/paltaie/romannumerals/RomanNumeralServiceImpl.java
@@ -3,11 +3,36 @@ package com.paltaie.romannumerals;
 public class RomanNumeralServiceImpl implements RomanNumeralService {
 
     public int romanToDecimal(String roman) {
-        return 0;
+        int result = 0;
+
+        long ms = roman.chars().filter(c -> c == 'M').count();
+        long ds = roman.chars().filter(c -> c == 'D').count();
+        long ls = roman.chars().filter(c -> c == 'L').count();
+        long cs = roman.chars().filter(c -> c == 'C').count();
+        long xs = roman.chars().filter(c -> c == 'X').count();
+        long vs = roman.chars().filter(c -> c == 'V').count();
+        long is = roman.chars().filter(c -> c == 'I').count();
+
+        if (roman.contains("IV")) result -= 2;
+        if (roman.contains("IX")) result -= 2;
+        if (roman.contains("XL")) result -= 10;
+        if (roman.contains("CD")) result -= 200;
+        if (roman.contains("CM")) result -= 200;
+
+        result += ms * 1000;
+        result += ds * 500;
+        result += ls * 50;
+        result += cs * 100;
+        result += xs * 10;
+        result += vs * 5;
+        result += is;
+
+        return result;
     }
 
     @Override
     public String decimalToRoman(int decimal) {
+
         return "I";
     }
 }
